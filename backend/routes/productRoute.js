89f99fe8808dcc27268529ce495746b18bd41a64
yujit2003 <yujityadav99@gmail.com>
2023-02-13 22:34:45 +0530

    first commit

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const { getAllProducts } = require('../controller/projectController');
-
-
 const router = express.Router();
+const { getAllProducts, createProduct, UpdateProduct, deleteProduct, getProductDetails} = require('../controller/projectController');
+const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
+
+
 
 router.route("/products").get(getAllProducts);
+
+router.route("/admin/products/new").post(isAuthenticatedUser,authorizeRoles("admin"),createProduct);
+
+router.route("/admin/product/:id")
+.put(isAuthenticatedUser,authorizeRoles("admin"),UpdateProduct)
+.delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct)
+
+router.route("/product/:id").get(getProductDetails);
+
+
+
 console.log("productrouter")
 
 module.exports = router;
